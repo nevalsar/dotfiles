425a0ce29d2ba16b6578e272bff34b91e2a90a18
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2021-10-29 15:30:11 -0400

    Move to zsh-snap for plugin management
    For all its power, zinit is a headache to work with.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias open=xdg-open
+
+# Git aliases
+#
+alias gb='git branch'
+alias gba='git branch --all --verbose'
+alias gc='git commit --verbose'
+alias gco='git checkout'
+alias gws='git status --short'
+alias gwS='git status'
+alias gwd='git diff --no-ext-diff'
+alias gwdD='git diff --no-ext-diff --word-diff'
+alias gs='git stash'
+alias gsa='git stash apply'
+alias gia='git add'
+alias gid='git diff --no-ext-diff --cached'
+alias giD='git diff --no-ext-diff --cached --word-diff'
+alias gl='git log --topo-order'
+
