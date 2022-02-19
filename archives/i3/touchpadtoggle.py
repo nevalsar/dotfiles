5b7c3d69056ddeeb7ecd276e08f0b0574a58e6ff
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2022-02-18 20:31:33 -0500

    Remove archives folder
    Delete unused archives folder.

--- archives/i3/touchpadtoggle.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import subprocess, sys
-
-status = subprocess.Popen("synclient -l | grep 'TouchpadOff' | cut -d '=' -f 2", shell=True, stdout=subprocess.PIPE).communicate()[0]
-status = str(1 - int(status))
-subprocess.call(["synclient", "TouchpadOff="+status])
--- archives/i3/wallpaper_picker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-files=(~/Pictures/Wallpapers/*)
-echo ${files[RANDOM % ${#files[@]}]} | xargs -I file ln -s -f file ~/.i3/wallpaper
-exit 0
