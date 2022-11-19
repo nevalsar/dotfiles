e0f794187cc4b9ce5751ba52b4bcd5746d2d73d7
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2022-11-19 17:28:12 -0500

    Add additional git aliases
    Add aliases for remote update and more logging types.

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -20,18 +20,35 @@ alias kubectl="minikube kubectl --"
 
 # Git aliases
 #
+
+# Branch (b)
 alias gb='git branch'
 alias gba='git branch --all --verbose'
+
+# Commit (c)
 alias gc='git commit --verbose'
 alias gco='git checkout'
+
+# Working copy (ws)
 alias gws='git status --short'
 alias gwS='git status'
 alias gwd='git diff --no-ext-diff'
 alias gwdD='git diff --no-ext-diff --word-diff'
+
+# Remote (R)
+alias gRu='git remote update'
+
+# Stash (s)
 alias gs='git stash'
 alias gsa='git stash apply'
+
+# Index (i)
 alias gia='git add'
 alias gid='git diff --no-ext-diff --cached'
 alias giD='git diff --no-ext-diff --cached --word-diff'
+
+# Log (l)
 alias gl='git log --topo-order'
+alias glc='git shortlog --summary --numbered'
+alias glS='git log --show-signature'
 
