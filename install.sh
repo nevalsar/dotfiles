e9badfb642020af7dc1c58d41d9eadd9a0d0a305
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2024-09-18 20:08:44 -0700

    feat: add simple installer script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cat banner.txt
+
+ln -s zsh/zprofile ${HOME}/.zprofile
+ln -s zsh/zshrc ${HOME}/.zshrc
+ln -s zsh/zshenv ${HOME}/.zshenv
+ln -s zsh/zsh_plugins.txt ${HOME}/.zsh_plugins.txt
+
