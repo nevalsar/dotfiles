f955a13f561913b7ae3b726332cb513d70f518da
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2024-09-18 20:55:46 -0700

    feat: use simplistic path to CWD for script

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 cat banner.txt
 
-ln -s zsh/zprofile ${HOME}/.zprofile
-ln -s zsh/zshrc ${HOME}/.zshrc
-ln -s zsh/zshenv ${HOME}/.zshenv
-ln -s zsh/zsh_plugins.txt ${HOME}/.zsh_plugins.txt
+echo "Installing ..."
 
+DOTFILES_DIR=$(pwd)
+
+ln -s ${DOTFILES_DIR}/zsh/zprofile ${HOME}/.zprofile
+ln -s ${DOTFILES_DIR}/zsh/zshrc ${HOME}/.zshrc
+ln -s ${DOTFILES_DIR}/zsh/zshenv ${HOME}/.zshenv
+ln -s ${DOTFILES_DIR}/zsh/zsh_plugins.txt ${HOME}/.zsh_plugins.txt
